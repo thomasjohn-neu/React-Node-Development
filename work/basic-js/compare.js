0bd00d43224569ec7cd27c28f742972e29acb335
Thomas John <john.th@northeastern.edu>
2022-05-30 23:30:14 -0400

    assignment basic js

--- work/basic-js/compare.js
+++ work/basic-js/compare.js
@@ -3,8 +3,20 @@
 module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 
 function compare( word, guess ) {  // DO NOT MODIFY
-
-/* YOU MAY MODIFY THE LINES BELOW */
-
-  return 0; // this line is wrong
+  let counter = 0;
+  let matches = {}
+  // comparing character by charatcer and updating the matches object with matching characters
+  for(let i=0; i < word.length; i++){
+    for(let j=0; j < guess.length; j++){
+      if(word.charAt(i).toLowerCase()===guess.charAt(j).toLowerCase()){
+        // check for key in matches, increment occurence if found, otherwise assign with 1
+        matches[word.charAt(i).toLowerCase()] = matches[word.charAt(i).toLowerCase()] ? matches[word.charAt(i).toLowerCase()]++ : 1;
+      }
+    }
+  }
+  // counting the matching charatcers from matches object
+  for ( let match in matches ){
+    counter++;
+  }
+  return counter;
 }
